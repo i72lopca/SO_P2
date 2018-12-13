05d8eeefd3d4a6fc9d5b1bfa22e4b34b6148d56b
Andres Lopez Cardenas <i72lopca@uco.es>
2018-12-13 14:34:49 +0100

    fix

--- ej3/ej3_3.c
+++ ej3/ej3_3.c
@@ -3,17 +3,16 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define NPROD 5
-#define NCONS 10
+#define NCONS 5
 #define TAMBUF 10
-#define Nprod 20 //Numero de productos que se pruduciran y se consumiran
+#define Nprod 20 //Numero de productos que se pruduciran y se consumiran por cada hilo
 
 void *productor();
 void *consumidor();
 
 int bufer[TAMBUF];
 sem_t mutex, full, empty; //Declaro los semaforos necesarios para el algoritmo productor-consumidor
-int itP = 0, itC = 0, sumP = 0, sumC = 0; //Ahora como van a producir y consumir entre varios hilos hay compartir el iterador
+int itC = 0, sumP = 0, sumC = 0; //Ahora como van a producir y consumir entre varios hilos hay compartir el iterador
 
 int main(){
 //	inicializo los semaforos
@@ -22,20 +21,14 @@ int main(){
 	sem_init(&empty, 0, TAMBUF);
 
 	srand(time(NULL));
-	pthread_t idProd[NPROD], idCon[NCONS];
+	pthread_t idProd, idCon[NCONS];
 
-//	Creo los hilos que pruducen y los consumidores
-	for(int i=0; i<NPROD; i++){
-		pthread_create(&idProd[i], NULL, productor, NULL);
-	}
+	pthread_create(&idProd, NULL, productor, NULL);
 	for(int i=0; i<NCONS; i++){
 		pthread_create(&idCon[i], NULL, consumidor, NULL);
 	}
 	
-//	Recibo tanto los hilos productores como los consumidores
-	for(int i=0; i<NPROD; i++){
-		pthread_join(idProd[i], NULL);
-	}
+	pthread_join(idProd, NULL);
 	for(int i=0; i<NCONS; i++){
 		pthread_join(idCon[i], NULL);
 	}
@@ -48,16 +41,15 @@ int main(){
 void *productor(){
 	extern int bufer[TAMBUF];
 	extern sem_t mutex, full, empty;
-	extern int sumP, itP;
+	extern int sumP;
 	int val;
 
-	for(int i=0; i<(Nprod / NPROD); i++){
+	for(int i=0; i<(Nprod); i++){
 		val = rand()%1001;
 		sem_wait(&empty);
 		sem_wait(&mutex);
-		bufer[itP%TAMBUF] = val;
+		bufer[i%TAMBUF] = val;
 		sumP += val;
-		itP++;
 		sem_post(&mutex);
 		sem_post(&full);		
 	}
@@ -73,7 +65,6 @@ void *consumidor(){
 		sem_wait(&full);
 		sem_wait(&mutex);
 		sumC += bufer[itC%TAMBUF];
-		bufer[itC%TAMBUF] = 0;
 		itC++;
 		sem_post(&mutex);
 		sem_post(&empty);
